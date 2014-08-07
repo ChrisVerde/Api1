11fb4eb6746ba9a6ab6b04c6e15b1deb6cbcf8d4
ChrisVerde <christian.verde@live.com>
2014-08-07 18:36:00 +0200

    Initial commit

---
+++ apigility/data/statuslib.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+    'c7a3eacd-d8fb-4068-8afd-340add4abf7a' => array(
+        'message' => 'First post!',
+        'user' => 'mwop',
+        'id' => 'c7a3eacd-d8fb-4068-8afd-340add4abf7a',
+        'timestamp' => 1407237943,
+    ),
+    '0abbfc2a-8986-4bd7-ad9a-f253a15e466f' => array(
+        'message' => 'First post!',
+        'user' => 'mwop',
+        'id' => '0abbfc2a-8986-4bd7-ad9a-f253a15e466f',
+        'timestamp' => 1407237964,
+    ),
+    'e7eefcf6-8f24-4ce9-875d-bdc83592ab63' => array(
+        'message' => 'First post!',
+        'user' => 'mwop',
+        'id' => 'e7eefcf6-8f24-4ce9-875d-bdc83592ab63',
+        'timestamp' => 1407238706,
+    ),
+);
